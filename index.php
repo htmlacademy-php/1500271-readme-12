50b63c21ee230220c21a4beb429c15b6278d5b56
Alexander-grd <65712452+Alexander-grd@users.noreply.github.com>
2020-06-15 11:38:39 +0300

    Добавил шаблоны
    Добавил шаблоны

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+$is_auth = rand(0, 1);
+/*function textcut($str, $len) {
+if (strlen($str) > $len) {
+    $arr_str = explode(' ', $str);
+    
+}
+};
+if */
+$posts = [
+    [
+        'caption' => 'Цитата',
+        'type' => 'post-quote',
+        'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+        'user_name' => 'Лариса',
+        'avatar' => 'userpic-larisa-small.jpg'
+    ],
+    [
+        'caption' => 'Игра престолов',
+        'type' => 'post-text',
+        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+        'user_name' => 'Владик',
+        'avatar' => 'userpic.jpg'
+    ],
+    [
+        'caption' => 'Наконец, обработал фотки!',
+        'type' => 'post-photo',
+        'content' => 'rock-medium.jpg',
+        'user_name' => 'Виктор',
+        'avatar' => 'userpic-mark.jpg'
+    ],
+    [
+        'caption' => 'Моя мечта',
+        'type' => 'post-photo',
+        'content' => 'coast-medium.jpg',
+        'user_name' => 'Лариса',
+        'avatar' => 'userpic-larisa-small.jpg'
+    ],
+    [
+        'caption' => 'Лучшие курсы',
+        'type' => 'post-link',
+        'content' => 'www.htmlacademy.ru',
+        'user_name' => 'Владик',
+        'avatar' => 'userpic.jpg'
+    ]
+];
+$user_name = 'Александр'; // укажите здесь ваше имя
+$title = 'Популярное';
+require('templates/layout.php');
+?>
